4044633f6c97fd9dd6590969678df902a6fbedce
plentymonkey <10546500+plentymonkey@users.noreply.github.com>
2022-01-24 12:02:25 +0100

    Automated interface update 2022-01-24_12:02:25

--- Modules/Market/Ebay/AccountPolicy/Contracts/FulfillmentPolicyRepositoryContract.php
+++ Modules/Market/Ebay/AccountPolicy/Contracts/FulfillmentPolicyRepositoryContract.php
@@ -10,11 +10,50 @@ interface FulfillmentPolicyRepositoryContract
 {
 
 	/**
+	 * Creates a new fulfillment policy.
+	 */
+	public function create(
+		array $data
+	):FulfillmentPolicy;
+
+	/**
 	 * Get a stored fulfillment policy based on ID.
 	 */
 	public function get(
 		string $id, 
 		array $filters = []
 	):FulfillmentPolicy;
+
+	/**
+	 * Get a stored fulfillment policy based on ID.
+	 */
+	public function getPolicies(
+		int $credentialsId, 
+		string $marketplaceId
+	):array;
+
+	/**
+	 * Updates a new fulfillment policy.
+	 */
+	public function update(
+		string $id, 
+		array $data
+	):FulfillmentPolicy;
+
+	/**
+	 * Deletes a fulfillment policy.
+	 */
+	public function delete(
+		string $id, 
+		array $data
+	):bool;
+
+	/**
+	 * Deletes a fulfillment policies.
+	 */
+	public function deleteBulk(
+		int $userId, 
+		int $marketId
+	):bool;
 
 }
