1d4855fc2760a7b4e8e0efde1f3ccc003804f639
plentybuildmonkey <cicd+plenty-cicd-3@plentymarkets.com>
2022-02-15 12:12:46 +0000

    Automated interface update 2022-02-15_12:12:46

--- Modules/Pim/SearchService/Filter/SupplierFilter.php
+++ Modules/Pim/SearchService/Filter/SupplierFilter.php
@@ -4,9 +4,11 @@ namespace Plenty\Modules\Pim\SearchService\Filter;
 use Illuminate\Contracts\Support\Arrayable;
 use Plenty\Modules\Cloud\ElasticSearch\Lib\ElasticSearch;
 use Plenty\Modules\Cloud\ElasticSearch\Lib\Query\Statement\Filter\TermFilter;
+use Plenty\Modules\Cloud\ElasticSearch\Lib\Query\Statement\Filter\TermsFilter;
 use Plenty\Modules\Cloud\ElasticSearch\Lib\Query\Statement\Filter\WildcardStringFilter;
 use Plenty\Modules\Cloud\ElasticSearch\Lib\Query\Statement\StatementInterface;
 use Plenty\Modules\Cloud\ElasticSearch\Lib\Query\Type\Filter\BoolMustFilter;
+use Plenty\Modules\Cloud\ElasticSearch\Lib\Query\Type\Filter\BoolMustNotFilter;
 use Plenty\Modules\Cloud\ElasticSearch\Lib\Query\Type\TypeInterface;
 
 /**
@@ -19,6 +21,34 @@ abstract class SupplierFilter implements TypeInterface
 	abstract public function matchSupplierItemNumber(
 		string $term, 
 		string $precision = \Plenty\Modules\Cloud\ElasticSearch\Lib\ElasticSearch::SEARCH_TYPE_EXACT
+	):self;
+
+	/**
+	 * Restricts the result to have the supplier id.
+	 */
+	abstract public function hasSupplier(
+		int $supplierId
+	):self;
+
+	/**
+	 * Restricts the result to have any of the tag ids.
+	 */
+	abstract public function hasAnySupplier(
+		array $tagIds
+	):self;
+
+	/**
+	 * Restricts the result to have all of the tag ids.
+	 */
+	abstract public function hasAllSuppliers(
+		array $supplierIds
+	):self;
+
+	/**
+	 * Restricts the result to not match the specified IDs.
+	 */
+	abstract public function hasNotSupplierIds(
+		array $supplierIds
 	):self;
 
 	abstract public function toArray(
