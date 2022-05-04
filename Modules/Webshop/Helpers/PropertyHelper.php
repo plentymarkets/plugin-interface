18cc4040c2a323ba894c0bbf78163eb44ae8af40
plentybuildmonkey <cicd+plenty-cicd-3@plentymarkets.com>
2022-05-04 00:14:37 +0000

    Automated interface update 2022-05-04_00:14:37

---
+++ Modules/Webshop/Helpers/PropertyHelper.php
@@ -0,0 +1,17 @@
+<?php
+namespace Plenty\Modules\Webshop\Helpers;
+
+use Plenty\Modules\Core\Data\Factories\LazyLoaderFactory;
+use Plenty\Modules\Property\V2\Models\Property;
+
+/**
+ * A helper class that lazyloads a property by the given property id.
+ */
+abstract class PropertyHelper 
+{
+
+	abstract public static function getPropertyById(
+		int $propertyId
+	);
+
+}
