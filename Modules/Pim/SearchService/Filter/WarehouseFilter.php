e3f0079ce28920209067d11b27d07e7ba83cd1b4
plentybuildmonkey <cicd+plenty-cicd-3@plentymarkets.com>
2022-08-01 12:10:29 +0000

    Automated interface update 2022-08-01_12:10:29

---
+++ Modules/Pim/SearchService/Filter/WarehouseFilter.php
@@ -0,0 +1,51 @@
+<?php
+namespace Plenty\Modules\Pim\SearchService\Filter;
+
+use Illuminate\Contracts\Support\Arrayable;
+use Plenty\Modules\Cloud\ElasticSearch\Lib\Query\Statement\Filter\TermFilter;
+use Plenty\Modules\Cloud\ElasticSearch\Lib\Query\Statement\Filter\TermsFilter;
+use Plenty\Modules\Cloud\ElasticSearch\Lib\Query\Statement\StatementInterface;
+use Plenty\Modules\Cloud\ElasticSearch\Lib\Query\Type\Filter\BoolMustFilter;
+use Plenty\Modules\Cloud\ElasticSearch\Lib\Query\Type\Filter\BoolMustNotFilter;
+use Plenty\Modules\Cloud\ElasticSearch\Lib\Query\Type\TypeInterface;
+
+/**
+ * Includes filters for warehouse
+ */
+abstract class WarehouseFilter implements TypeInterface
+
+{
+
+	/**
+	 * Restricts the result to have the warehouse ID.
+	 */
+	abstract public function isInWarehouse(
+		int $warehouseId
+	):WarehouseFilter;
+
+	/**
+	 * Restricts the result to not match the specified IDs.
+	 */
+	abstract public function hasNotWarehouseIds(
+		array $warehouseIds
+	):WarehouseFilter;
+
+	/**
+	 * Restricts the result to have the any of the warehouse IDs.
+	 */
+	abstract public function hasWarehouseIds(
+		array $warehouseIds
+	):WarehouseFilter;
+
+	abstract public function toArray(
+	):array;
+
+	abstract public function addStatement(
+		StatementInterface $statement
+	);
+
+	abstract public function addQuery(
+		 $statement
+	);
+
+}
