eda7eec4fa142802fe2130c4a19327abeee5c347
plentybuildmonkey <cicd+plenty-cicd-3@plentymarkets.com>
2023-01-06 00:17:17 +0000

    Automated interface update 2023-01-06_00:17:17

---
+++ Modules/Webshop/Contracts/CountryRepositoryContract.php
@@ -0,0 +1,20 @@
+<?php
+namespace Plenty\Modules\Webshop\Contracts;
+
+use Plenty\Modules\Order\Shipping\Countries\Models\Country;
+
+/**
+ * Repository to get and set country information for the webshop
+ */
+interface CountryRepositoryContract 
+{
+
+	public function getEUCountriesList(
+		string $lang
+	):array;
+
+	public function getActiveCountriesList(
+		string $lang
+	):array;
+
+}
