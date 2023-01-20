470714933dca3715e08ef87d38ab02fdaf2a6146
plentybuildmonkey <cicd+plenty-cicd-3@plentymarkets.com>
2023-01-20 00:17:04 +0000

    Automated interface update 2023-01-20_00:17:04

--- Modules/Messenger/Services/NewMessengerStatusService.php
+++ Modules/Messenger/Services/NewMessengerStatusService.php
@@ -8,7 +8,7 @@ namespace Plenty\Modules\Messenger\Services;
 abstract class NewMessengerStatusService 
 {
 
-	const ALLOWED_PIDS = [51054,40650,54545];
+	const ALLOWED_PIDS = [51054,40650,54545,48250,52261,53033,54173,54802,55054,56153,52469,61426,61653];
 
 	abstract public static function isActive(
 	);
