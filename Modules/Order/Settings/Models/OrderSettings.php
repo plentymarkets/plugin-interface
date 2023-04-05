bc4cefd0542ebc7a1a1fa00ca496d0157dddc32e
plentybuildmonkey <cicd+plenty-cicd-3@plentymarkets.com>
2023-04-05 12:10:13 +0000

    Automated interface update 2023-04-05_12:10:13

--- Modules/Order/Settings/Models/OrderSettings.php
+++ Modules/Order/Settings/Models/OrderSettings.php
@@ -8,17 +8,55 @@ namespace Plenty\Modules\Order\Settings\Models;
 abstract class OrderSettings 
 {
 
+	const DEFAULT_VALUE = 'defaultValue';
+
+	const VALUE_IS_NULLABLE = 'valueIsNullable';
+
+	const DIVERGENT_DB_KEY = 'divergentDBKey';
+
+	const LOAD_OPTION_FIELD_SEPARATELY = 'loadOptionFieldSeparately';
+
 	const CREATED_AT = 'createdAt';
 
 	const UPDATED_AT = 'updatedAt';
 	
 public		$warehouseAssignment;
 	
+public		$warehouseSelection;
+	
+public		$orderItemPrefixes;
+	
 public		$cancelStatusFrom;
 	
 public		$cancelStatusTo;
 	
-public		$orderItemPrefixes;
+public		$reservationStatusFrom;
+	
+public		$reservationStatusTo;
+	
+public		$reservationOnlyPaidOrders;
+	
+public		$noStockStatus4;
+	
+public		$stockChangeStatusFrom;
+	
+public		$stockChangeStatusTo;
+	
+public		$defaultVariationIdForDunningFees;
+	
+public		$defaultReferrerId;
+	
+public		$showInactivePaymentMethods;
+	
+public		$downPayment;
+	
+public		$useNewOrderUi;
+	
+public		$oldUiDisplayAddress;
+	
+public		$oldUiDisplayPricesNet;
+	
+public		$oldUiChangeableBundleQuantity;
 	
 	/**
 	 * Returns this model as an array.
