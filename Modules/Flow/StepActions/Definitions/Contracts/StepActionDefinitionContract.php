d74a89036bcf045bb5992e2aed6fa7784de0060f
plentybuildmonkey <cicd+plenty-cicd-3@plentymarkets.com>
2023-06-08 00:16:49 +0000

    Automated interface update 2023-06-08_00:16:49

---
+++ Modules/Flow/StepActions/Definitions/Contracts/StepActionDefinitionContract.php
@@ -0,0 +1,52 @@
+<?php
+namespace Plenty\Modules\Flow\StepActions\Definitions\Contracts;
+
+use Exception;
+use Illuminate\Translation\Translator;
+use Plenty\Modules\Flow\Models\Input;
+use Plenty\Modules\Flow\Models\Output;
+use Plenty\Modules\Flow\StepActions\Definitions\Exceptions\StepActionDefinitionException;
+use Plenty\Modules\Flow\StepActions\Definitions\Models\UI\StepActionUI;
+use Plenty\Modules\Fulfillment\DataModels\ConfigForm\FormField;
+
+/**
+ * Flow step action
+ */
+abstract class StepActionDefinitionContract 
+{
+
+	abstract public function getIdentifier(
+	):string;
+
+	abstract public function getName(
+	):string;
+
+	abstract public function getDescription(
+	):string;
+
+	abstract public function getUIAction(
+	):StepActionUI;
+
+	abstract public function getUIConfigFields(
+	):array;
+
+	abstract public function getRequiredInputTypes(
+	):array;
+
+	abstract public function getProvidedOutputTypes(
+	):array;
+
+	abstract public function performTask(
+		array $inputs, 
+		array $configFields
+	):array;
+
+	abstract public function validateConfigFields(
+		array $configFields
+	);
+
+	abstract public function validateInputs(
+		 $inputs
+	);
+
+}
---
+++ Modules/Flow/StepActions/Definitions/Models/UI/StepActionUI.php
@@ -0,0 +1,11 @@
+<?php
+namespace Plenty\Modules\Flow\StepActions\Definitions\Models\UI;
+
+
+/**
+ * A flow UI step action provides the information that is necessary for an action to be displayed in the UI.
+ */
+abstract class StepActionUI 
+{
+
+}
