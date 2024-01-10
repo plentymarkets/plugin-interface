2291c352751de36e542755dcc0ed0340564c8353
plentybuildmonkey <cicd+plenty-cicd-3@plentymarkets.com>
2024-01-10 12:07:22 +0000

    Automated interface update 2024-01-10_12:07:22

---
+++ Modules/Document/Builder/Variables/Models/Groups/ContactGroup.php
@@ -0,0 +1,22 @@
+<?php
+namespace Plenty\Modules\Document\Builder\Variables\Models\Groups;
+
+
+/**
+ * The contact variables group
+ */
+abstract class ContactGroup 
+{
+
+	const IDENTIFIER = 'Contact';
+
+	/**
+	 * Returns this model as an array.
+	 */
+	public function toArray(
+	):array
+	{
+		return [];
+	}
+
+}
