5b3c75b38b987338d8b60d0df0c4348be94800f6
plentybuildmonkey <cicd+plenty-cicd-3@plentymarkets.com>
2024-01-17 12:06:30 +0000

    Automated interface update 2024-01-17_12:06:30

---
+++ Modules/Document/Builder/Variables/Models/Groups/ContactBankDataGroup.php
@@ -0,0 +1,22 @@
+<?php
+namespace Plenty\Modules\Document\Builder\Variables\Models\Groups;
+
+
+/**
+ * The ContactBankData variables group
+ */
+abstract class ContactBankDataGroup 
+{
+
+	const IDENTIFIER = 'ContactBankData';
+
+	/**
+	 * Returns this model as an array.
+	 */
+	public function toArray(
+	):array
+	{
+		return [];
+	}
+
+}
