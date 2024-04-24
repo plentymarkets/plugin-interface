0c1ded740f38fbeeb68670e6cd8669a61a9627fc
plentybuildmonkey <cicd+plenty-cicd-3@plentymarkets.com>
2024-04-24 12:07:20 +0000

    Automated interface update 2024-04-24_12:07:20

---
+++ Modules/Document/Builder/Variables/Models/Groups/MessengerGroup.php
@@ -0,0 +1,22 @@
+<?php
+namespace Plenty\Modules\Document\Builder\Variables\Models\Groups;
+
+
+/**
+ * The Messenger variables group
+ */
+abstract class MessengerGroup 
+{
+
+	const IDENTIFIER = 'Messenger';
+
+	/**
+	 * Returns this model as an array.
+	 */
+	public function toArray(
+	):array
+	{
+		return [];
+	}
+
+}
