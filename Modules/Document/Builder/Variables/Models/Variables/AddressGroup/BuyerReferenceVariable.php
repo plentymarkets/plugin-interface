6e18c6d9d8c18f27960914e2aae374b9a61d0cec
plentybuildmonkey <cicd+plenty-cicd-3@plentymarkets.com>
2024-07-12 00:14:52 +0000

    Automated interface update 2024-07-12_00:14:52

---
+++ Modules/Document/Builder/Variables/Models/Variables/AddressGroup/BuyerReferenceVariable.php
@@ -0,0 +1,22 @@
+<?php
+namespace Plenty\Modules\Document\Builder\Variables\Models\Variables\AddressGroup;
+
+
+/**
+ * The buyer reference address option variable
+ */
+abstract class BuyerReferenceVariable 
+{
+
+	const IDENTIFIER = 'BuyerReference';
+
+	/**
+	 * Returns this model as an array.
+	 */
+	public function toArray(
+	):array
+	{
+		return [];
+	}
+
+}
