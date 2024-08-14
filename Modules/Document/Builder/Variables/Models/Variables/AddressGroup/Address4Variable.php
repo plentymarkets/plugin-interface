924d4ab4ae4345165f5fb5f1bc717235cf2aa419
plentybuildmonkey <cicd+plenty-cicd-3@plentymarkets.com>
2024-08-14 12:07:58 +0000

    Automated interface update 2024-08-14_12:07:58

--- Modules/Document/Builder/Variables/Models/Variables/AddressGroup/Address4Variable.php
+++ Modules/Document/Builder/Variables/Models/Variables/AddressGroup/Address4Variable.php
@@ -10,6 +10,8 @@ abstract class Address4Variable
 
 	const IDENTIFIER = 'Address4';
 
+	const MANUAL_URL = ['https://knowledge.plentymarkets.com/de-de/manual/main/auftraege/documentbuilder-variablen-verwenden.html','https://knowledge.plentymarkets.com/en-gb/manual/main/orders/documentbuilder-using-variables.html'];
+
 	/**
 	 * Returns this model as an array.
 	 */
--- Modules/Document/Builder/Variables/Models/Variables/AddressGroup/AddressBaseVariable.php
+++ Modules/Document/Builder/Variables/Models/Variables/AddressGroup/AddressBaseVariable.php
@@ -8,6 +8,8 @@ namespace Plenty\Modules\Document\Builder\Variables\Models\Variables\AddressGrou
 abstract class AddressBaseVariable 
 {
 
+	const MANUAL_URL = ['https://knowledge.plentymarkets.com/de-de/manual/main/auftraege/documentbuilder-variablen-verwenden.html','https://knowledge.plentymarkets.com/en-gb/manual/main/orders/documentbuilder-using-variables.html'];
+
 	/**
 	 * Returns this model as an array.
 	 */
