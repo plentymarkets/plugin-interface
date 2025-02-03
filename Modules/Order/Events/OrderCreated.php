cb910ccef471556952a4369bef31068ca64461c2
plentybuildmonkey <cicd+plenty-cicd-3@plentymarkets.com>
2025-02-03 12:07:26 +0000

    Automated interface update 2025-02-03_12:07:26

--- Modules/Order/Events/OrderCreated.php
+++ Modules/Order/Events/OrderCreated.php
@@ -5,7 +5,7 @@ use Plenty\Modules\Order\Models\Order;
 
 /**
  * An event class fired after a new order is created. The order type is not relevant.
- * 	At the same time also type depended events will be fired, like ReturnsCreated or CreditNoteCreated.
+ *  At the same time also type depended events will be fired, like ReturnsCreated or CreditNoteCreated.
  */
 abstract class OrderCreated extends \Plenty\Modules\Order\Events\OrderEvent 
 
--- Modules/Order/Events/SubscriptionChildOrderCreated.php
+++ Modules/Order/Events/SubscriptionChildOrderCreated.php
@@ -5,7 +5,7 @@ use Plenty\Modules\Order\Models\Order;
 
 /**
  * An event class fired after a new order for a subscription is created. The order type is not relevant.
- * 	At the same time also type depended events will be fired, like ReturnsCreated or CreditNoteCreated.
+ *  At the same time also type depended events will be fired, like ReturnsCreated or CreditNoteCreated.
  */
 abstract class SubscriptionChildOrderCreated extends \Plenty\Modules\Order\Events\OrderEvent 
 
