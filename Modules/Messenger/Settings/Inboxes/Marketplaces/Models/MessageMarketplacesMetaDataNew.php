d4f635ce439199b2c4daec7d8c82bb171500e666
plentybuildmonkey <cicd+plenty-cicd-3@plentymarkets.com>
2025-07-02 12:08:01 +0000

    Automated interface update 2025-07-02_12:08:01

---
+++ Modules/Messenger/Settings/Inboxes/Marketplaces/Models/MessageMarketplacesMetaDataNew.php
@@ -0,0 +1,22 @@
+<?php
+namespace Plenty\Modules\Messenger\Settings\Inboxes\Marketplaces\Models;
+
+
+/**
+ * The marketplaces data model of the messenger.
+ */
+abstract class MessageMarketplacesMetaDataNew 
+{
+	
+public		$ebayProperties;
+	
+	/**
+	 * Returns this model as an array.
+	 */
+	public function toArray(
+	):array
+	{
+		return [];
+	}
+
+}
